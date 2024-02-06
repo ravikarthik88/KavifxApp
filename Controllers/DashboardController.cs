8540d546f45dc3a333fd059958ae9543a895a5bf
ravikarthik88 <32273326+ravikarthik88@users.noreply.github.com>
2024-02-06 14:42:11 +0530

    Add project files.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using KavifxApp.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Common;
+using System.Text;
+
+namespace KavifxApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KavifxApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
