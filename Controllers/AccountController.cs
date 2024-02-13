415741901129bfc4d75e92505cc9bfb68fdc565e
ravikarthik88 <32273326+ravikarthik88@users.noreply.github.com>
2024-02-13 08:34:01 +0530

    Git changes on 12-02-24

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,150 @@
+﻿using KavifxApp.Models.DTO;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using Microsoft.Win32;
+using System.Net.Http;
+
+namespace KavifxApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        HttpClient client;
+        IConfiguration config;
+        private readonly ILogger<HomeController> _logger;
+        public AccountController(IHttpClientFactory factory, IConfiguration cfg, ILogger<HomeController> logger) 
+        {
+            client = factory.CreateClient();
+            config = cfg;
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            string Token = string.Empty;
+            string BaseUrl = config.GetSection("ApiUrl:Dev").Value;
+
+            LoginViewModel login = new LoginViewModel()
+            {
+                Email = model.Email,
+                Password = model.Password
+            };
+
+            var ReqContent = JsonContent.Create(login);
+            var response = await client.PostAsync(BaseUrl + "Account/Login", ReqContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Token = await response.Content.ReadAsStringAsync();
+                var roles = User.Claims.Where(c => c.Type == "role").Select(c => c.Value).ToList();
+                
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email,model.Email)                    
+                };
+               
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                HttpContext.Session.SetString("JWTToken", Token);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody]RegisterViewModel viewModel) 
+        {
+            string Token = HttpContext.Session.GetString("JWTToken");
+            string BaseUrl = config.GetSection("ApiUrl:Dev").Value;
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Token);
+            RegisterViewModel register = new RegisterViewModel
+            {
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Email = viewModel.Email,
+                Password = viewModel.Password,
+                ConfirmPassword = viewModel.ConfirmPassword
+            };
+
+            var Request = JsonContent.Create(register);
+            var response = await client.PostAsync(BaseUrl + "Account/Register", Request);
+            if (response.IsSuccessStatusCode)
+            {
+                var JsonString = await response.Content.ReadAsStringAsync();
+                var User = JsonConvert.SerializeObject(JsonString);
+                ViewBag.User = User;
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            string Token = HttpContext.Session.GetString("JWTToken");
+            string BaseUrl = config.GetSection("ApiUrl:Dev").Value;
+            if (!string.IsNullOrEmpty(Token))
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Token);
+                var response = await client.GetAsync(BaseUrl + "User");
+                if (response.IsSuccessStatusCode)
+                {
+                    var JsonString = await response.Content.ReadAsStringAsync();
+                    var UserLst = JsonConvert.DeserializeObject<List<RegisterViewModel>>(JsonString);
+                    ViewBag.Ust = UserLst;
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(UserProfileViewModel model)
+        {
+            string Token = HttpContext.Session.GetString("JWTToken");
+            string BaseUrl = config.GetSection("ApiUrl:Dev").Value;
+
+            var multipartContent = new MultipartFormDataContent();
+            multipartContent.Add(new StringContent(model.FirstName), "FirstName");
+            multipartContent.Add(new StringContent(model.LastName), "LastName");
+            multipartContent.Add(new StringContent(model.Email), "Email");
+            multipartContent.Add(new StringContent(model.PhoneNumber), "PhoneNumber");
+            multipartContent.Add(new StringContent(model.OrganizationName), "OrganizationName");
+            multipartContent.Add(new StringContent(model.Location), "Location");
+            multipartContent.Add(new StringContent(model.DateOfBirth), "DateOfBirth");
+            multipartContent.Add(new StreamContent(model.ProfilePicture.OpenReadStream()), "file", model.ProfilePicture.FileName);
+
+            var response = await client.PostAsync(BaseUrl + "", multipartContent);
+            if (response.IsSuccessStatusCode)
+            {
+                // Success
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                // Handle error
+                return RedirectToAction("Error");
+            }
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ Models/ViewModel/kavifxViewModels.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KavifxApp.Models.DTO
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required,Compare("Password"),DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }        
+
+    }
+
+    public class UserProfileViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string OrganizationName { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required, DataType(DataType.Date)]
+        public string DateOfBirth { get; set; }
+        [Required, DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [NotMapped]
+        public IFormFile ProfilePicture { get; set; }
+        public byte[] ProfilePictureData { get; set; }
+        public string ProfilePictureUrl { get; set; }
+    }
+
+
+    public class RoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+
+    public class PermissionViewModel
+    {
+        public string PermissionName { get; set; }
+    }
+
+    public class AssignRoleToUserViewModel
+    {
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    public class UpdateRoleToUserViewModel
+    {
+        public string Email { get; set; }
+        public List<string> RoleNames { get; set; }
+    }
+
+    public class RolePermissionViewModel
+    {
+        public string RoleName { get; set; }
+        public string PermissionName { get; set; }
+    }
+
+    public class UpdateRolePermissionModel
+    {
+        public string RoleName { get; set; }
+        public List<string> PermissionNames { get; set; }
+    }
+
+   
+}
